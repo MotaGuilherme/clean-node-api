bc3258b28daca403de948f594a4c84281c0d45f2
MotaGuilherme <metatronmota@gmail.com>
2023-09-13 11:44:50 -0300

    feat: ensure SingUpcontroller returns 400 if no password is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,10 +1,10 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from "../errors/missing-param-error";
-import {badRequest} from "../helpers/http-helper";
+import { badRequest } from "../helpers/http-helper";
 
 export class SingUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
         for (const field of requiredFields) {
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
