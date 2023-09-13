747d804cc2b39eede69e7674f8b70dd4cf904aa9
MotaGuilherme <metatronmota@gmail.com>
2023-09-13 17:32:41 -0300

    refactor: move all protocol imports to a single file

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,5 +1,5 @@
 import { SingUpController } from './singup'
-import {EmailValidator} from "../protocols/email-validator";
+import { EmailValidator } from "../protocols";
 import { MissingParamError, InvalidParamError, ServerError } from "../errors";
 
 
