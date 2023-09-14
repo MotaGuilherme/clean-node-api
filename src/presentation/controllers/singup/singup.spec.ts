5a3fa09d974c6ac0bd57d6823afa94e20e0282ae
MotaGuilherme <metatronmota@gmail.com>
2023-09-14 02:40:54 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/singup/singup.spec.ts
+++ src/presentation/controllers/singup/singup.spec.ts
@@ -221,4 +221,25 @@ describe('SingUp Controller', () => {
         expect(httpResponse.body).toEqual(new ServerError())
 
     })
+
+    // @ts-ignore
+    test('Should return 200 if invalid data is provided', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                name: 'valid_name',
+                email: 'valid_email@mail.com',
+                password: 'valid_password',
+                passwordConfirmation: 'valid_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'valid_password'
+        })
+    })
 })
