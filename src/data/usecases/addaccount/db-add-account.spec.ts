9c8a7bc7478e385d92b1b3c8c66eb94961ceda0e
MotaGuilherme <metatronmota@gmail.com>
2023-09-18 22:06:45 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/addaccount/db-add-account.spec.ts
+++ src/data/usecases/addaccount/db-add-account.spec.ts
@@ -103,4 +103,23 @@ describe('DbAddAccount Usecase', () => {
         const promise = sut.add(accountData)
         await expect(promise).rejects.toThrow()
     })
+
+    // @ts-ignore
+    test('Should returns and account on success', async () => {
+        const { sut} = makeSut()
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_mail@mail.com',
+            password: 'valid_password'
+        }
+        const account = await sut.add(accountData)
+        expect(account).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_mail@mail.com',
+            password: 'valid_password'
+        })
+    })
+
+
 })
