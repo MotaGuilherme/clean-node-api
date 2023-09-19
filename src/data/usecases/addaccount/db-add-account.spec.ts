549f028cd937b5557da178ea56be523e11115876
MotaGuilherme <metatronmota@gmail.com>
2023-09-18 22:02:08 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/addaccount/db-add-account.spec.ts
+++ src/data/usecases/addaccount/db-add-account.spec.ts
@@ -74,20 +74,33 @@ describe('DbAddAccount Usecase', () => {
     //     await expect(promise).rejects.toThrow()
     // })
 
+    // // @ts-ignore
+    // test('Should call AddAccount Repository with correct values', async () => {
+    //     const { sut, addAccountRepositoryStub } = makeSut()
+    //     const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
+    //     const accountData = {
+    //         name: 'valid_name',
+    //         email: 'valid_mail@mail.com',
+    //         password: 'valid_password'
+    //     }
+    //     await sut.add(accountData)
+    //     expect(addSpy).toHaveBeenCalledWith({
+    //         name: 'valid_name',
+    //         email: 'valid_mail@mail.com',
+    //         password: 'hashed_password'
+    //     })
+    // })
+
     // @ts-ignore
     test('Should call AddAccount Repository with correct values', async () => {
         const { sut, addAccountRepositoryStub } = makeSut()
-        const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
+        jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
         const accountData = {
             name: 'valid_name',
             email: 'valid_mail@mail.com',
             password: 'valid_password'
         }
-        await sut.add(accountData)
-        expect(addSpy).toHaveBeenCalledWith({
-            name: 'valid_name',
-            email: 'valid_mail@mail.com',
-            password: 'hashed_password'
-        })
+        const promise = sut.add(accountData)
+        await expect(promise).rejects.toThrow()
     })
 })
