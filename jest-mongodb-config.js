6fb375f205bea5634e01cf82918bc5b0ca5fcbcf
MotaGuilherme <metatronmota@gmail.com>
2023-09-21 14:26:37 -0300

    feat: ensure AccountMongoRepository return on success

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    getMongodbMemoryServerOptions: {
-        instance: {
-            dbName: 'jest'
-        },
+    mongodbMemoryServerOptions: {
         binary: {
             version: '4.0.3',
-            skipMD5: true
+            skipMD5: true,
         },
-        autoStart: false
-    }
-}
+        autoStart: false,
+        instance: {},
+    },
+};
