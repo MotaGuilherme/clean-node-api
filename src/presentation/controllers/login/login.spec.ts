439ffad9c4a87220ea5ad6b3f64e5f01e402d4ca
MotaGuilherme <metatronmota@gmail.com>
2023-09-27 17:36:42 -0300

    test: ensure LoginController returns 400 if password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -15,4 +15,16 @@ describe('Login Controller', () => {
         const httpResponse = await sut.handle(httpResquest)
         expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
     })
+
+    // @ts-ignore
+    test('Should return if no password id provided ', async () => {
+        const sut = new LoginController()
+        const httpResquest = {
+            body: {
+                email: 'any_email@mail.com'
+            }
+        }
+        const httpResponse = await sut.handle(httpResquest)
+        expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+    })
 })
