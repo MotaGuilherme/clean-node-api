7aed842dffad7ad50079b152e26e0217c58ee6b8
MotaGuilherme <metatronmota@gmail.com>
2023-09-28 18:49:17 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { LoginController } from "./login";
-import { badRequest, serverError, unauthorized } from "../../helpers/http-helper";
+import { badRequest, ok, serverError, unauthorized } from "../../helpers/http-helper";
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { HttpRequest, EmailValidator, Authentication } from "./login-protocols";
 
@@ -122,5 +122,12 @@ describe('Login Controller', () => {
         jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
         const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(serverError(new Error()))
+    })
+
+    // @ts-ignore
+    test('Should returns 200 if valid credentials are provided', async () => {
+        const { sut} = makeSut()
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
     })
 })
