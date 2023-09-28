4e5df3fd78e78f9fcdf5dd0685b1219b44f62789
MotaGuilherme <metatronmota@gmail.com>
2023-09-28 17:30:28 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { InvalidParamError, MissingParamError } from "../../errors";
-import {badRequest, serverError} from "../../helpers/http-helper";
+import { badRequest, serverError } from "../../helpers/http-helper";
 import { EmailValidator } from "../../protocols/email-validator";
 import { Authentication } from "../../../domain/usecases/authentication";
 
@@ -13,17 +13,17 @@ export class LoginController implements Controller {
     }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
-            const {email, password} = httpRequest.body
+            const requiredFields = [ 'email', 'password' ]
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
+            }
+            const { email, password } = httpRequest.body
 
-            if (!email) {
-                return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
-            }
-            if (!password) {
-                return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
-            }
             const isValid = this.emailValidator.isValid(email)
             if (!isValid) {
-                return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+                return badRequest(new InvalidParamError('email'))
             }
             await this.authentication.auth(email, password)
         } catch (error) {
