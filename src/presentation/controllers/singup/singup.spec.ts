f835b423516394f391b25b3fba9785d97f4b835b
MotaGuilherme <metatronmota@gmail.com>
2023-09-28 19:10:45 -0300

    feat: ensure SingUpController returns 400 if Validation fails

--- src/presentation/controllers/singup/singup.spec.ts
+++ src/presentation/controllers/singup/singup.spec.ts
@@ -217,4 +217,13 @@ describe('SingUp Controller', () => {
         await sut.handle(makeFakeRequest())
         expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
     })
+
+    // @ts-ignore
+    test('Should return 400 if Validation returns an error', async () => {
+        const { sut, validationStub } = makeSut()
+        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+    })
+
 })
