6d3839a5d1b52eaa9c5cd4acf8b76346d28ee3e8
MotaGuilherme <metatronmota@gmail.com>
2023-10-06 15:58:49 -0300

    test: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -49,4 +49,12 @@ describe('Bcrypter adapter', () => {
         await sut.compare('any_value', 'any_hash')
         expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
     })
+
+    // @ts-ignore
+    test('Should return true when compare succeeds', async () => {
+        const sut = makeSut()
+        const isValid = await sut.compare('any_value', 'any_hash')
+        expect(isValid).toBe(true)
+    })
+
 })
