b186e1e7d686950b8a0e39ab372fbd827077e403
MotaGuilherme <metatronmota@gmail.com>
2023-10-11 17:31:19 -0300

    feat: add Login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { makeSingUpController } from "../factories/singup/singup-validation-factory";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
+import { makeLoginController } from "../factories/login/login-factory";
 
 export default (router: Router): void => {
-    router.post('/singup', adaptRoute(makeSingUpController()))
+    router.post('/signup', adaptRoute(makeSingUpController()))
+    router.post('/login', adaptRoute(makeLoginController()))
 }
